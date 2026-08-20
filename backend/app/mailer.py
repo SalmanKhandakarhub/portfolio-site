3f4d364701e28d2c8e4176cba79480e2c280bae1
Salman <salman@pitangent.com>
2026-08-20 13:08:27 +0530

    SMPT port for 465

--- backend/app/mailer.py
+++ backend/app/mailer.py
@@ -54,9 +54,8 @@ async def _send(
         port=settings.SMTP_PORT,
         username=settings.SMTP_USER,
         password=settings.SMTP_PASSWORD,
-        use_tls=False,       # False for port 587
-        start_tls=True,      # True for port 587
-        timeout=20,
+        use_tls=True,  # Must be True for 465
+        start_tls=False,  # Must be False for 465
     )
 
 
